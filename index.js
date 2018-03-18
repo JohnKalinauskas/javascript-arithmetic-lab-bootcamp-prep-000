24b071dd00eab62777659a69550020f78e5beab2
John Kalinauskas <jkalinauskas@live.com>
2018-03-18 23:25:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,36 @@
-
+var number;
+function add(a, b){ //add a and b and return the result.
+  number = a + b;
+  return number;
+}
+function subtract(a, b){ //subtract a and b and return the result.
+  number = a - b;
+  return number;
+}
+function multiply(a, b)
+  number = a * b;
+  return number;
+}
+function divide(a, b){
+  number = a / b;
+  return number;
+}
+function inc(a){ 
+  number = a + 1;
+  return number;
+}
+function dec(a){ 
+  number = a - 1;
+  return number;
+}
+function makeInt(a){  integer.
+  number = parseInt(a, 10);
+  return number;
+}
+function preserveDecimal(a){ //Parse a as a float (number with decimals) and return that number.
+  number = parseFloat(a, 10);
+  return number;
+}
 
 
 
